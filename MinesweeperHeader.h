49ebdd55c6b8d97ce04916b6bd5472d4ee087e07
Ilyasnow <Ilyasnow@i.ua>
2015-06-02 23:21:40 +0300

    Create MinesweeperHeader.h

---
+++ MinesweeperHeader.h
@@ -0,0 +1,9 @@
+#include<windows.h>
+#include<time.h>
+//#include <CommCtrl.h>
+ 
+//#pragma comment(lib,"ComCtl32.Lib")
+
+#define SCREEN_X_TILES 30
+#define SCREEN_Y_TILES 20
+#define MINE_NUMBER 60
